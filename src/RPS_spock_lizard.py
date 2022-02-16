4b323586097f1fe3c66220de06c129dafdcbdc7f
dfleta <gelpiorama@gmail.com>
2022-02-16 20:22:27 +0100

    refactor(assess_game): Generalizar la funcion a una clase

--- src/RPS_spock_lizard.py
+++ src/RPS_spock_lizard.py
@@ -34,89 +34,96 @@ Victories = {
 
 NUMBER_RECENT_ACTIONS = 5
 
+class Game:
 
-def assess_game(user_action, computer_action):
-    game_result = None
-    
-    if user_action == computer_action:
-        print(f"User and computer picked {user_action.name}. Draw game!")
-        game_result = GameResult.Tie
+    def __init__(self):
+        self.game_history = []
+        self.user_actions_history = []
 
-    elif computer_action in Victories[user_action]:
-        print("%s wins %s. You lost!" %(computer_action.name, user_action.name))
-        game_result = GameResult.Defeat
+    def append_user_actions_history(self, action):
+        self.user_actions_history.append(action)
 
-    else:
-        print("%s wins %s. You win!" %(user_action.name, computer_action.name))
-        game_result = GameResult.Victory
+    def append_game_history(self, game_result):
+        self.game_history.append(game_result)
 
-    return game_result
+    def assess_game(self):
+        game_result = None
+        
+        if self.user_action == self.computer_action:
+            print(f"User and computer picked {self.user_action.name}. Draw game!")
+            game_result = GameResult.Tie
 
-            
-def get_computer_action(user_actions_history, game_history):
-    # No previous user actions => random computer choice
-    if not user_actions_history or not game_history:
-        computer_action = get_random_computer_action(GameAction)
-    # Alternative AI functionality
-    # Choice that would beat the user's most frequent recent choice
-    else:
-        most_frequent_recent_computer_action = GameAction(mode(user_actions_history[-NUMBER_RECENT_ACTIONS:]))
-        computer_action = get_winner_action(most_frequent_recent_computer_action)
+        elif self.computer_action in Victories[self.user_action]:
+            print("%s wins %s. You lost!" %(self.computer_action.name, self.user_action.name))
+            game_result = GameResult.Defeat
 
-    print(f"Computer picked {computer_action.name}.")
-    
-    return computer_action
-            
+        else:
+            print("%s wins %s. You win!" %(self.user_action.name, self.computer_action.name))
+            game_result = GameResult.Victory
 
-def get_user_action():
-    # Scalable to more options (beyond rock, paper and scissors...)
-    game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
-    game_choices_str = ", ".join(game_choices)
-    user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
-    user_action = GameAction(user_selection)
-       
-    return user_action
+        return game_result
 
+                
+    def get_computer_action(self):
+        # No previous user actions => random computer choice
+        if not self.user_actions_history or not self.game_history:
+            computer_action = self.get_random_computer_action(GameAction)
+        # Alternative AI functionality
+        # Choice that would beat the user's most frequent recent choice
+        else:
+            most_frequent_recent_computer_action = GameAction(mode(self.user_actions_history[-NUMBER_RECENT_ACTIONS:]))
+            computer_action = self.get_winner_action(most_frequent_recent_computer_action)
 
-def get_random_computer_action(actions):
-    computer_selection = random.randint(0, len(actions) - 1)
-    computer_action = actions[computer_selection]
+        print(f"Computer picked {computer_action.name}.")
+        
+        return computer_action
+                
 
-    return computer_action
-
-
-def get_winner_action(game_action):
-    winner_actions = Victories[game_action]
-    return get_random_computer_action(winner_actions)    
+    def get_user_action(self):
+        # Scalable to more options (beyond rock, paper and scissors...)
+        game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
+        game_choices_str = ", ".join(game_choices)
+        user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
+        user_action = GameAction(user_selection)
+        
+        return user_action
 
 
-def play_another_round():
-        another_round = input("\nAnother round? (y/n): ")
-        return another_round.lower() == 'y'
+    def get_random_computer_action(self, actions):
+        computer_selection = random.randint(0, len(actions) - 1)
+        computer_action = actions[computer_selection]
+
+        return computer_action
+
+
+    def get_winner_action(self, game_action):
+        winner_actions = Victories[game_action]
+        return self.get_random_computer_action(winner_actions)    
+
+
+    def play_another_round(self):
+            another_round = input("\nAnother round? (y/n): ")
+            return another_round.lower() == 'y'
         
 
-def main():
-    game_history = []
-    user_actions_history = []
+if __name__ == "__main__":
+    
+    game = Game()
     
     while True:
-        computer_action = get_computer_action(user_actions_history, game_history)
+
+        computer_action = game.get_computer_action()
         
         try:
-            user_action = get_user_action()
-            user_actions_history.append(user_action)
+            user_action = game.get_user_action()
+            game.append_user_actions_history(user_action)
         except ValueError as e:
             range_str = f"[0, {len(GameAction) - 1}]"
             print(f"Invalid selection. Pick a choice in range {range_str}!")
             continue
 
+        game_result = game.assess_game()
+        game.append_game_history.append(game_result)
 
-        game_result = assess_game(user_action, computer_action)
-        game_history.append(game_result)
-
-        if not play_another_round():
+        if not game.play_another_round():
             break
-        
-
-if __name__ == "__main__":
-    main()
