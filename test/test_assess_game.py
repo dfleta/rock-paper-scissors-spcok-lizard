4287fd568a203f1fad7354dce22b8ed726616592
David Gelpi <gelpiorama@gmail.com>
2024-12-09 14:48:42 +0100

    style(test_assess_game): Formatear codigo

--- test/test_assess_game.py
+++ test/test_assess_game.py
@@ -9,23 +9,23 @@ def game():
 
 def test_draw(game):
     assert GameResult.Tie == game.assess_game(
-        user_action=GameAction.Spock, 
+        user_action=GameAction.Spock,
         computer_action=GameAction.Spock)
-    
+
     assert GameResult.Tie == game.assess_game(
-        user_action=GameAction.Lizard, 
+        user_action=GameAction.Lizard,
         computer_action=GameAction.Lizard)
 
     assert GameResult.Tie == game.assess_game(
-        user_action=GameAction.Rock, 
+        user_action=GameAction.Rock,
         computer_action=GameAction.Rock)
 
     assert GameResult.Tie == game.assess_game(
-        user_action=GameAction.Scissors, 
+        user_action=GameAction.Scissors,
         computer_action=GameAction.Scissors)
 
     assert GameResult.Tie == game.assess_game(
-        user_action=GameAction.Paper, 
+        user_action=GameAction.Paper,
         computer_action=GameAction.Paper)
 
 
@@ -35,11 +35,11 @@ def test_spock_loses(game):
     Spock pierde con Lizard y Paper 
     '''
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Paper, 
+        user_action=GameAction.Paper,
         computer_action=GameAction.Spock)
 
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Lizard, 
+        user_action=GameAction.Lizard,
         computer_action=GameAction.Spock)
 
 
@@ -49,11 +49,11 @@ def test_spock_wins(game):
     Spock gana a Rock y Scissors 
     '''
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Rock, 
+        user_action=GameAction.Rock,
         computer_action=GameAction.Spock)
 
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Scissors, 
+        user_action=GameAction.Scissors,
         computer_action=GameAction.Spock)
 
 
@@ -63,11 +63,11 @@ def test_lizard_loses(game):
     Lizard pierde con Rock y Scissors 
     '''
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Rock, 
+        user_action=GameAction.Rock,
         computer_action=GameAction.Lizard)
 
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Scissors, 
+        user_action=GameAction.Scissors,
         computer_action=GameAction.Lizard)
 
 
@@ -77,11 +77,11 @@ def test_lizard_wins(game):
     Lizard gana a Spock y Paper 
     '''
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Spock, 
+        user_action=GameAction.Spock,
         computer_action=GameAction.Lizard)
 
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Paper, 
+        user_action=GameAction.Paper,
         computer_action=GameAction.Lizard)
 
 
@@ -90,11 +90,11 @@ def test_rock_loses(game):
     Rock pierde con Spock y Paper 
     '''
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Spock, 
+        user_action=GameAction.Spock,
         computer_action=GameAction.Rock)
 
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Paper, 
+        user_action=GameAction.Paper,
         computer_action=GameAction.Rock)
 
 
@@ -103,11 +103,11 @@ def test_rock_wins(game):
     Rock gana a Scissors y Lizard 
     '''
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Scissors, 
+        user_action=GameAction.Scissors,
         computer_action=GameAction.Rock)
 
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Lizard, 
+        user_action=GameAction.Lizard,
         computer_action=GameAction.Rock)
 
 
@@ -116,11 +116,11 @@ def test_paper_loses(game):
     Paper pierde con Scissors y Lizard 
     '''
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Scissors, 
+        user_action=GameAction.Scissors,
         computer_action=GameAction.Paper)
 
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Lizard, 
+        user_action=GameAction.Lizard,
         computer_action=GameAction.Paper)
 
 
@@ -129,11 +129,11 @@ def test_paper_wins(game):
     Paper gana a Rock y Spock 
     '''
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Rock, 
+        user_action=GameAction.Rock,
         computer_action=GameAction.Paper)
 
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Spock, 
+        user_action=GameAction.Spock,
         computer_action=GameAction.Paper)
 
 
@@ -142,11 +142,11 @@ def test_scissors_loses(game):
     Scissors pierde con Spock y Rock 
     '''
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Spock, 
+        user_action=GameAction.Spock,
         computer_action=GameAction.Scissors)
 
     assert GameResult.Victory == game.assess_game(
-        user_action=GameAction.Rock, 
+        user_action=GameAction.Rock,
         computer_action=GameAction.Scissors)
 
 
@@ -155,11 +155,11 @@ def test_scissors_wins(game):
     Scissors gana a Lizard y Paper 
     '''
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Lizard, 
+        user_action=GameAction.Lizard,
         computer_action=GameAction.Scissors)
 
     assert GameResult.Defeat == game.assess_game(
-        user_action=GameAction.Paper, 
+        user_action=GameAction.Paper,
         computer_action=GameAction.Scissors)
 
 
@@ -167,11 +167,11 @@ def test_scissors_wins(game):
 def test_minus_action():
 
     assert 1 == len(GameAction.minus(
-        GameAction.Scissors, 
+        GameAction.Scissors,
         GameAction.Lizard,
         GameAction.Paper,
         GameAction.Rock))
-    
+
     assert 4 == len(GameAction.minus(GameAction.Lizard))
 
     assert GameAction.Lizard not in GameAction.minus(GameAction.Lizard)
